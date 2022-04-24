979cc58c6c9dcc29ddd8ef0ad3bcbb1e67f447bd
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2022-04-24 06:40:34 +0200

    01 knapsack

--- main.go
+++ main.go
@@ -4,62 +4,74 @@ import (
 	"log"
 )
 
+type cell struct {
+	combinedVal int
+	itemsUsed   []int
+}
+
 func KnapsackProblem(items [][]int, capacity int) []interface{} {
 	var combinedValsNumRows = len(items)
 	var combinedValsNumCols = capacity + 1
-	var combinedVals = make([][]int, combinedValsNumRows)
-	var sliceToSlice = make([]int, combinedValsNumRows*combinedValsNumCols)
+	var combinedVals = make([][]cell, combinedValsNumRows)
+	var sliceToSlice = make([]cell, combinedValsNumRows*combinedValsNumCols)
 	for i := range combinedVals {
 		combinedVals[i] = sliceToSlice[:combinedValsNumCols]
 		sliceToSlice = sliceToSlice[combinedValsNumCols:]
 	}
 
-	//var indexesOfItemsUsed []int
-
-	log.Println(combinedVals)
 	for i := 0; i < len(items); i++ {
 		var currItemVal = items[i][0]
 		var currItemWeight = items[i][1]
 
 		for currCapacity := 0; currCapacity < combinedValsNumCols; currCapacity++ {
 			if currCapacity == 0 {
-				combinedVals[i][currCapacity] = 0
+				combinedVals[i][currCapacity].combinedVal = 0
 				continue
 			}
 
 			if i == 0 {
 				if currCapacity >= currItemWeight {
-					combinedVals[i][currCapacity] = currItemVal
+					combinedVals[i][currCapacity] = cell{
+						combinedVal: currItemVal,
+						itemsUsed:   append(combinedVals[i][currCapacity].itemsUsed, i),
+					}
 					continue
 				}
-				combinedVals[i][currCapacity] = 0
+				combinedVals[i][currCapacity].combinedVal = 0
 				continue
 			}
 
 			var combinedValUsingCurrItem int
 
 			if currCapacity-currItemWeight >= 0 {
-				combinedValUsingCurrItem = combinedVals[i-1][currCapacity-currItemWeight] + currItemVal
+				combinedValUsingCurrItem = combinedVals[i-1][currCapacity-currItemWeight].combinedVal + currItemVal
 			}
 
-			var combinedValWithoutUsingCurrItem = combinedVals[i-1][currCapacity]
+			var combinedValWithoutUsingCurrItem = combinedVals[i-1][currCapacity].combinedVal
+			var itemsUsed = combinedVals[i-1][currCapacity].itemsUsed
 
 			var maxCurrCombinedVal = combinedValWithoutUsingCurrItem
 
 			if combinedValUsingCurrItem > combinedValWithoutUsingCurrItem {
 				maxCurrCombinedVal = combinedValUsingCurrItem
+				itemsUsed = append(combinedVals[i-1][currCapacity-currItemWeight].itemsUsed, i)
 			}
 
-			combinedVals[i][currCapacity] = maxCurrCombinedVal
+			combinedVals[i][currCapacity] = cell{
+				combinedVal: maxCurrCombinedVal,
+				itemsUsed:   itemsUsed,
+			}
 		}
 	}
 
-	log.Println(combinedVals)
+	if combinedVals[combinedValsNumRows-1][combinedValsNumCols-1].itemsUsed == nil {
+		combinedVals[combinedValsNumRows-1][combinedValsNumCols-1].itemsUsed = []int{}
+	}
 
 	// Replace return below.
 	return []interface{}{
-		combinedVals[combinedValsNumRows-1][combinedValsNumCols-1], // total value
-		[]int{1, 2}, // item indices
+		combinedVals[combinedValsNumRows-1][combinedValsNumCols-1].combinedVal, // total value
+		combinedVals[combinedValsNumRows-1][combinedValsNumCols-1].itemsUsed,   // item indices
 	}
 }
 
